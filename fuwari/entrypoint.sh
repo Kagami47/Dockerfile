eed6d199295c33cef4770c002c821757566031ff
Lie <79914543+Kagami47@users.noreply.github.com>
2025-09-01 10:17:23 +0800

    Update entrypoint.sh

--- fuwari/entrypoint.sh
+++ fuwari/entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 # 初始化挂载目录（如果为空）
 init_directory() {
     if [ -d "$RUN_DIR" ] && [ -z "$(ls -A "$RUN_DIR")" ]; then
-        echo "Initializing $RUN_DIR with default content from $PROJECT_DIR"
-        cp -r "$PROJECT_DIR"/* "$RUN_DIR"/
+        echo "正在从$PROJECT_DIR初始化到$RUN_DIR"
+        cp -a "$PROJECT_DIR"/. "$RUN_DIR"/
     fi
 }
 
@@ -13,7 +13,7 @@ init_directory() {
 init_directory
 
 # 切换到运行目录
-cd $RUN_DIR
+cd ${RUN_DIR}
 
 # 启动命令
 if [ "$1" = "bash" ]; then
